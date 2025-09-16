8ec03b3790476bcd34e0c9cb192998dde832ac09
Murat Kocacık <113637437+muratkocacik@users.noreply.github.com>
2025-09-16 17:57:25 +0300

    Update config.py

--- HTTP Status Tester/config.py
+++ HTTP Status Tester/config.py
@@ -1,7 +1,4 @@
 # config.py
-
 URL_LIST = [
-    "https://www.example.com",
-    "https://www.google.com",
-    "https://www.yahoo.com",
+    {"url": "https://www.example.com/301","expected": "https://www.example.com/200"}
 ]
